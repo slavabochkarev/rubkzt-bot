f04aa2be280601460158be1813e61be85096470e
Вячеслав Евгеньевич Бочкарев <70199356+slavabochkarev@users.noreply.github.com>
2025-08-06 10:34:04 +0500

    Fix output

--- handlers/converter.py
+++ handlers/converter.py
@@ -32,10 +32,10 @@ def try_convert_amount(message: str, data: dict) -> str | None:
             kzt_per_1_rub = 1 / rub_per_1_kzt
                        
             converted = round(amount / kzt_per_1_rub, 2)
-            line_cb = f"💰 {amount} {currency_code} ({name}) / {kzt_per_1_rub:.4f} = {converted} RUB"
+            line_cb = f"💰 По курсу ЦБ {amount} {currency_code} / {kzt_per_1_rub:.4f} = {converted} RUB"
             if local_rate and local_rate > 0:
                 converted_local = round(amount / local_rate, 2)
-                line_local = f"💰 {amount} {currency_code} / {local_rate:.4f} = {converted_local} RUB"
+                line_local = f"По обменному курсу {amount} {currency_code} / {local_rate:.4f} = {converted_local} RUB"
                 return f"{line_cb}\n{line_local}"
             else:
                 return line_cb
