4850f7b7f4b3436b47dc6469f51cdf72b4e3b9c1
Вячеслав Евгеньевич Бочкарев <70199356+slavabochkarev@users.noreply.github.com>
2025-08-30 13:27:56 +0500

    Small changes1

--- bot_1.py
+++ bot_1.py
@@ -751,7 +751,7 @@ async def main():
     app.add_handler(CommandHandler("codes", codes))
     #app.add_handler(CommandHandler("checkchrome", checkchrome))	
     #app.add_handler(MessageHandler(filters.TEXT & filters.Regex("коды валют"), codes))
-    app.add_handler(MessageHandler(filters.TEXT & filters.Regex(pattern_code, flags=re.IGNORECASE),codes)
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex(pattern_code, flags=re.IGNORECASE),codes))
     app.add_handler(MessageHandler(filters.TEXT & filters.Regex("статистика"), stat_activ))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.Regex("коды валют"), echo))
 
@@ -776,6 +776,7 @@ if __name__ == "__main__":
     except RuntimeError as e:
         if "cannot close a running event loop" not in str(e).lower():
             raise
+
 
 
 
